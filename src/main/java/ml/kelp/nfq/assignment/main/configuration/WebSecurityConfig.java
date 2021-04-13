48e968551486b244458d5c679945df0441e8fbd8
Nojus R <nojusr@gmail.com>
2021-04-13 21:15:27 +0300

    added bootstrap js, touched up templates, added proper security config

--- src/main/java/ml/kelp/nfq/assignment/main/configuration/WebSecurityConfig.java
+++ src/main/java/ml/kelp/nfq/assignment/main/configuration/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/appointment", "/css").permitAll()
+                .antMatchers("/", "/appointment", "/css", "/js").permitAll()
                 .antMatchers("/home").authenticated()
                 .and()
                 .formLogin()
